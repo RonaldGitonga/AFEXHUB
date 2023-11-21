2682aa920354c91c2e698a87e6a2393453ca6b9d
RonaldGitonga <rgatuiku@gmail.com>
2023-11-21 14:36:37 +0300

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,27 @@
+//LANDING PAGE
+'use client'
+import React from 'react'
+import Headerone from './component/headerone/Main'
+import About from './component/menhome/About'
+import Courses from './component/menhome/Courses'
+import Event from './component/menhome/Event'
+import Scholarship from './component/menhome/Scholarship'
+import Shep from './component/menhome/Shep'
+import Slider from './component/menhome/Sliderone'
+import Frequently from './component/menhome/Frequently'
+import Video from './component/menhome/Video'
+import Testimonial from './component/menhome/Testimonial'
+import Search from './component/menhome/Search'
+import Admission from './component/menhome/Admission'
+import Redslider from './component/menhome/Redslider'
+import Blog from './component/menhome/Blog'
+import Subscribe from './component/menhome/Subscribe'
+const Homepage = () => {
+  return (
+    <div>  
+  Homepage
+</div>
+  )
+}
+
+export default Homepage
